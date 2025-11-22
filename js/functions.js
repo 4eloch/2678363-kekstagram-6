794a020174b4916e01a488a4dc6e8aedd66b1c7b
Murad Salikhov <murad.27012@yandex.ru>
2025-11-22 16:11:24 +0700

    First task

--- js/functions.js
+++ js/functions.js
@@ -1,41 +1,49 @@
-// function stringLength(str, num) {
-//   str.length <= num ? true : false;
-// }
-
-// stringLength("Зло – это зло, Стрегобор, – серьезно сказал ведьмак, вставая", 100);
-// stringLength("Лютик, бл**ь", 12);
-// stringLength("Время стирает воспоминания, искажает их", 10);
-
-
-
-// function palindrom(myStr) {
-//   let normalizeStr = myStr.replaceAll(" ", "").toLowerCase();
-//   let newStr = "";
-//   for (let i = normalizeStr.length - 1; i >= 0; i--) {
-//     newStr += normalizeStr[i];
-//   }
-//   newStr === normalizeStr ? console.log(true) : console.log(false);
-// }
-
-// palindrom("Топот");
-// palindrom("ДовОд");
-// palindrom("Ведьмак");
-// palindrom("Лёша на полке клопа нашёл ");
-
-
-
-function numberfromString(mixedString) {
-  let str = mixedString.toString();
-  console.log(str);
-  let numberString = "";
-  for (let i = 0; i < str; i++) {
-    numberString += str[i];
+function stringLength(str, num) {
+  if (str.length <= num) {
+    return true;
   }
-  console.log(numberString);
+  return false;
 }
 
-numberfromString('2023 год');            // 2023
-numberfromString('ECMAScript 2022');     // 2022
-numberfromString('1 кефир, 0.5 батона'); // 105
-numberfromString('агент 007');           // 7
-numberfromString('а я томат');           // NaN
+stringLength('Зло – это зло, Стрегобор, – серьезно сказал ведьмак, вставая', 100);
+stringLength('Лютик, бл**ь', 12);
+stringLength('Время стирает воспоминания, искажает их', 10);
+
+
+function palindrom(myStr) {
+  const normalizeStr = myStr.replaceAll(' ', '').toLowerCase();
+  let newStr = '';
+  for (let i = normalizeStr.length - 1; i >= 0; i--) {
+    newStr += normalizeStr[i];
+  }
+  if (normalizeStr === newStr) {
+    return true;
+  }
+  return false;
+}
+
+palindrom('Топот');
+palindrom('ДовОд');
+palindrom('Кекс');
+palindrom('Лёша на полке клопа нашёл ');
+
+
+function digitsFromString(mixedString) {
+  const str = mixedString.toString();
+  let digits = '';
+  for (let i = 0; i < str.length; i++) {
+    if (!Number.isNaN(parseInt(str[i], 10))) {
+      digits += str[i];
+    }
+  }
+  if (digits === '') {
+    return NaN;
+  }
+  return parseInt(digits, 10);
+}
+
+digitsFromString('2023 год');
+digitsFromString('ECMAScript 2022');
+digitsFromString('1 кефир, 0.5 батона');
+digitsFromString('агент 007');
+digitsFromString('а я томат');
